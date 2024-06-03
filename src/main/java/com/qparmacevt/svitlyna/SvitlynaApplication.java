050bf12472f5ee056f1439f1942e4f8be8985572
Oleh Melnyk <53297882+qparmacevt@users.noreply.github.com>
2024-06-03 12:18:59 +0300

    Spring REST API template added

---
+++ src/main/java/com/qparmacevt/svitlyna/SvitlynaApplication.java
@@ -0,0 +1,13 @@
+package com.qparmacevt.svitlyna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvitlynaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvitlynaApplication.class, args);
+	}
+
+}
